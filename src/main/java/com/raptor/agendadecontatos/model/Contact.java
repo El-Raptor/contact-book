8cbe82163884606e3a038303144fb57a7d8deee6
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-12 16:57:36 -0400

    feat(contact): add JPA and Lombok annotiations

--- src/main/java/com/raptor/agendadecontatos/model/Contact.java
+++ src/main/java/com/raptor/agendadecontatos/model/Contact.java
@@ -1,12 +1,19 @@
 package com.raptor.agendadecontatos.model;
 
-import lombok.Getter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Contact {
 
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
     private String name;
     private String email;
     private String phone;
