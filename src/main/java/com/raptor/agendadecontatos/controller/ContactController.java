dc07a67df0aaaf625f688934597d6870a531e3ff
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-12 17:37:52 -0400

    feat(contact): implement ContactController with CRUD operations.

---
+++ src/main/java/com/raptor/agendadecontatos/controller/ContactController.java
@@ -0,0 +1,39 @@
+package com.raptor.agendadecontatos.controller;
+
+import com.raptor.agendadecontatos.model.Contact;
+import com.raptor.agendadecontatos.service.ContactService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
+public class ContactController {
+    private final ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts() {
+        return contactService.findAll();
+    }
+
+    @GetMapping("contacts/{id}")
+    public Contact getContactById(@PathVariable int id) {
+        return contactService.findById(id);
+    }
+
+    @PostMapping("contact")
+    public void addContact(@RequestBody Contact contact) {
+        contactService.save(contact);
+    }
+
+    @PutMapping("contact")
+    public void updateContact(@RequestBody Contact contact) {
+        contactService.save(contact);
+    }
+
+}
