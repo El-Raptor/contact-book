3db59d34c1989d997ce3405369ff93629d385925
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-12 16:16:04 -0400

    chore: add initial files

---
+++ src/test/java/com/raptor/agendadecontatos/AgendaDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raptor.agendadecontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeContatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
