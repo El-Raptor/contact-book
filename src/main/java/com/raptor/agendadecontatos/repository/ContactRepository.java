20423d30e9692d696d64264bc63fd22816262b04
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-12 17:20:47 -0400

    feat(contact): implement ContactService with CRUD operations

--- src/main/java/com/raptor/agendadecontatos/repository/ContactRepository.java
+++ src/main/java/com/raptor/agendadecontatos/repository/ContactRepository.java
@@ -3,7 +3,9 @@ package com.raptor.agendadecontatos.repository;
 
 import com.raptor.agendadecontatos.model.Contact;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends CrudRepository<Contact,Integer> {
 
 }
